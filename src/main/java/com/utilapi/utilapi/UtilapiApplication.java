d20a94e86d98882188e3b6de01a66da27811b730
Juan Monsalve <juan.monsalve@cyxtera.com>
2019-04-20 21:06:45 -0500

    Clean Project. A template made of SpringBoot 2.1.4, JAVA8 and Maven technologies as basis

---
+++ src/main/java/com/utilapi/utilapi/UtilapiApplication.java
@@ -0,0 +1,13 @@
+package com.utilapi.utilapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utilapi/utilapi/UtilapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.utilapi.utilapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtilapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
