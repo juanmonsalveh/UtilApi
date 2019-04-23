c063f219bb2baf520cb4be759d30cc47ae65e628
Juan Monsalve <juan.monsalve@cyxtera.com>
2019-04-23 01:38:45 -0500

    Adding logging configuration (log4j) and adding exclusion to spring boot starter logging configuration to avoid issues

--- src/main/java/com/utilapi/utilapi/UtilapiApplication.java
+++ src/main/java/com/utilapi/utilapi/UtilapiApplication.java
@@ -1,13 +1,29 @@
 package com.utilapi.utilapi;
 
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UtilapiApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UtilapiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(UtilapiApplication.class, args);
-	}
+        BasicConfigurator.configure();
+
+        SpringApplication.run(UtilapiApplication.class, args);
+
+        LOGGER.trace("TRACE log for test");
+        LOGGER.debug("DEBUG log for test");
+        LOGGER.info("INFO log for test");
+        LOGGER.warn("WARN log for test");
+        LOGGER.error("ERROR log for test");
+
+
+    }
 
 }
