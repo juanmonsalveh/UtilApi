854d3fc010844519c583564c042445e196e7e1a2
juan.monsalve <juan.monsalve@cyxtera.com>
2019-05-07 20:13:32 -0500

    Adding Swagger configuration to document the API, Adding API general documentation

--- src/main/java/com/utilapi/com/simpleService/RootController.java
+++ src/main/java/com/utilapi/com/simpleService/RootController.java
@@ -1,5 +1,8 @@
 package com.utilapi.com.simpleService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,11 @@ public class RootController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
 
+//    @ApiOperation(value = "Returns a simple message to test the controller´s availability")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Ok", response = String.class),
+//            @ApiResponse(code = 500, message = "Service Error")
+//    })
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String rootMethod () {
         LOGGER.debug("Start rootMethod");
