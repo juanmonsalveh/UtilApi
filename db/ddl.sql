1357d464ecb0be45e3b0a69336b4446f6a8ae3a7
iroa <ivonne.roa@cyxtera.com>
2019-05-28 13:41:05 -0500

    * updating database script creation

--- db/ddl.sql
+++ db/ddl.sql
@@ -14,26 +14,27 @@ CREATE TABLE account (
   id       INT          NOT NULL
     PRIMARY KEY,
   balance  VARCHAR(255) NULL,
-  owner_id VARCHAR(255) NOT NULL,
+  owner_id INTEGER NOT NULL,
   FOREIGN KEY (owner_id) REFERENCES user_bank (id)
 );
 
 CREATE TABLE otp (
   otp_code  INT          NOT NULL
     PRIMARY KEY,
-  owner     VARCHAR(255) NOT NULL,
+  owner     INTEGER NOT NULL,
   FOREIGN KEY (owner) REFERENCES user_bank (id),
   is_active TINYINT(1)   NULL
 );
 
 CREATE TABLE transactions (
   id                  INT          NOT NULL PRIMARY KEY,
-  origin_account      VARCHAR(255) NOT NULL,
+  origin_account      INTEGER NOT NULL,
   FOREIGN KEY (origin_account) REFERENCES account (id),
-  destination_account VARCHAR(255) NOT NULL,
+  destination_account INTEGER NOT NULL,
   FOREIGN KEY (destination_account) REFERENCES account (id),
   amount              INTEGER      NOT NULL,
-  otp_code            VARCHAR(255) NOT NULL,
+  otp_code            INTEGER NOT NULL,
   FOREIGN KEY (otp_code) REFERENCES otp (otp_code),
   date                TIMESTAMP    NOT NULL
+
 );
